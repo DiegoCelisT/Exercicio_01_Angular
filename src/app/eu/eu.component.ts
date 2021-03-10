1cf158cdd2476ea7621715a0054b800f0a26f852
Diego Celis <diegofercelis@gmail.com>
2021-03-10 06:27:48 -0300

    Adicionado um easter egg bacana ;P

--- src/app/eu/eu.component.ts
+++ src/app/eu/eu.component.ts
@@ -14,14 +14,23 @@ export class EuComponent implements OnInit {
   descricaoDaMinhaFoto: any;
   minhaUrlDaFoto: any;
   cumprimento: any;
+  fantasminha: any;
+  PNGVazio: any;
 
   //Eventos mouseover e mouseout, deu trabalho mas AMEI!!!!!!!! <3
   Mouse: any = "OFF";
-  mouseNoDiego () {
+  mouseComDiego () {
     this.Mouse = "ON"
   }
   mouseSemDiego () {
     this.Mouse = "OFF"
+  }
+  Mouse2: any = "OFF";
+  mouseComBOO () {
+    this.Mouse2 = "ON"
+  }
+  mouseSemBOO () {
+    this.Mouse2 = "OFF"
   }
 
   constructor(eu: EuService) { 
@@ -31,6 +40,8 @@ export class EuComponent implements OnInit {
     this.descricaoDaMinhaFoto = eu.getPhotoDescription ();
     this.minhaUrlDaFoto = eu.getPhoto_url ();
     this.cumprimento = eu.getGreetings ();
+    this.fantasminha = eu.getBoo ();
+    this.PNGVazio = eu.getEmptyPNG ();
   }
 
   ngOnInit(): void {//DCT
--- src/app/services/eu.service.ts
+++ src/app/services/eu.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
-}) //DCT
+})
 export class EuService {
 
   name: any;
@@ -11,6 +11,8 @@ export class EuService {
   photo_description: any;
   photo_url: any;
   greetings: any;
+  ghost: any;
+  emptyPNG: any;
   
 
   constructor() { 
@@ -24,7 +26,9 @@ export class EuService {
     ];
     this.photo_description = "Este aí embaixo sou eu, brincando no tancão do AquaRio:";
     this.photo_url = "https://scontent.fsdu5-1.fna.fbcdn.net/v/t1.0-9/55869388_10156719801029024_9185843252572454912_o.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=c_rkAWTglZkAX_YO47R&_nc_ht=scontent.fsdu5-1.fna&oh=6510dbf0088fc5f93318a21fcc20ac51&oe=606B358F";
-    this.greetings = "https://www.pngkit.com/png/full/19-190719_hey-speech-bubble-png-campaa-de-prevencion-de.png"
+    this.greetings = "https://www.pngkit.com/png/full/19-190719_hey-speech-bubble-png-campaa-de-prevencion-de.png";
+    this.ghost = "https://64.media.tumblr.com/aec8ce5e5fcbca3e4958c5a317a0abb2/tumblr_mk0udxwKEy1s5zwimo1_400.gif";
+    this.emptyPNG = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/01e1a88f-c290-49fa-afe8-f4a6c31abe22/d4obm1n-37607dd5-93a8-43cc-b920-2c36e0304986.png/v1/fill/w_900,h_800,strp/fondo_png_vacio_by_carogugliottajb_d4obm1n-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MDAiLCJwYXRoIjoiXC9mXC8wMWUxYTg4Zi1jMjkwLTQ5ZmEtYWZlOC1mNGE2YzMxYWJlMjJcL2Q0b2JtMW4tMzc2MDdkZDUtOTNhOC00M2NjLWI5MjAtMmMzNmUwMzA0OTg2LnBuZyIsIndpZHRoIjoiPD05MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1mq0sjcQUOe6bkfdIdou1x82ZPj074-SlVtcRE6Pknw"
     
 }
 
@@ -50,6 +54,14 @@ export class EuService {
 
   getGreetings (): any {
     return this.greetings
+  }
+
+  getBoo (): any {
+    return this.ghost
+  }
+
+  getEmptyPNG (): any{
+    return this.emptyPNG
   }
 
 }
